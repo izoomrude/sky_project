dc632f0c818014625cfc0c477353351059e118d3
Аня <147255359+izoomrude@users.noreply.github.com>
2023-10-13 16:39:47 +0300

    add func in file str_func.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,13 @@
 
 def str_func():
+    """Все буквы заглавные"""
     user_str = input('Напишем капсом строчку: ')
     new_str = user_str.upper()
+    return new_str
+
+
+def first_letter_capital():
+    """ Каждая первая буква в слове заглавная """
+    user_str = input('Меняем строку: ')
+    new_str = user_str.title()
     return new_str
